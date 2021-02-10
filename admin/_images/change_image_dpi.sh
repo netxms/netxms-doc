8f104fe3d2996b2656a98ff1682b7077199c7dc4
Filipp Sudanov <filipp.sudanov@radensolutions.com>
2021-02-11 01:06:08 +0200

    Enforce . as decimal separator in change_image_dpi.sh

--- admin/_images/change_image_dpi.sh
+++ admin/_images/change_image_dpi.sh
@@ -2,9 +2,9 @@
 desired_dpi=150
 for f in *.png
 do
-   dpi=$(printf "%.0f\n" `identify -units PixelsPerInch -format "%x" $f`)
+   dpi=$(LC_NUMERIC=C printf "%.0f\n" "`identify -units PixelsPerInch -format "%x" $f`")
    if [[ "$dpi" != "$desired_dpi" ]]; then
       echo "Changing resolution of $f from $dpi to $desired_dpi"
       mogrify -units PixelsPerInch -density $desired_dpi $f
-   fi 
+   fi
 done

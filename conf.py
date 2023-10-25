72a3c4885694128de7be6aa81f5e11231e362561
Alex Kirhenshtein <alk@netxms.org>
2023-10-25 12:21:46 +0300

    Tracking code added

--- conf.py
+++ conf.py
@@ -57,6 +57,8 @@ html_show_sourcelink = False
 html_show_sphinx = False
 #html_show_copyright = True
 
+html_js_files = [('https://stats.raden.solutions/script.js', {'async': 'async', 'data-website-id':'e5a25886-8178-4d34-860f-f8cb9009a7e7'})]
+
 # -- Options for LaTeX output ---------------------------------------------
 #    'figure_align': 'H', - to avoid image floating to next page if it does not fit
 latex_elements = {

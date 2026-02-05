2431c98ce689f83cd9b7917d1cbefeb57250cb23
Filipp Sudanov <filipp.sudanov@radensolutions.com>
2026-02-05 13:06:47 +0200

    dpi changing script goes into subfolders

--- admin/_images/change_image_dpi.sh
+++ admin/_images/change_image_dpi.sh
@@ -1,10 +1,10 @@
 # mogrify is a part of imagemagic package
 desired_dpi=150
-for f in *.png
+find . -name '*.png' -type f -not -path './icons/*' | while read -r f
 do
-   dpi=$(LC_NUMERIC=C printf "%.0f\n" "`identify -units PixelsPerInch -format "%x" $f`")
+   dpi=$(LC_NUMERIC=C printf "%.0f\n" "$(identify -units PixelsPerInch -format "%x" "$f")")
    if [[ "$dpi" != "$desired_dpi" ]]; then
-      echo "Changing resolution of $f from $dpi to $desired_dpi"
-      mogrify -units PixelsPerInch -density $desired_dpi $f
+      echo "Changing dpi of $f from $dpi to $desired_dpi"
+      mogrify -units PixelsPerInch -density "$desired_dpi" "$f"
    fi
 done
